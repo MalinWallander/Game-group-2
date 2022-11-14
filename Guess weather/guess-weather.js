36bab0ab7244e668997a78923b9c6c4af2c8c43a
MalinW <maalinwallander@gmail.com>
2022-11-14 08:52:52 +0100

    Comments and JS, crashes sometimes

--- Guess weather/guess-weather.js
+++ Guess weather/guess-weather.js
@@ -1,7 +1,7 @@
 import { cities, apiKey } from "./constants.js";
-
+//Set random number
 let randNum = Math.floor(Math.random() * cities.length);
-
+//Set elements and objects
 let lat, long, cityName, kelvin, temp;
 let cityNameEl = document.getElementById('cityNamePresent');
 let submitButtonEl = document.getElementById('submit-button');
@@ -9,12 +9,14 @@ let refreshbuttonEl = document.getElementById('refresh-button');
 let answerEl = document.getElementById('answer');
 let form = document.forms.guess;
 
+//Get random city and data for the city
 const fetchCity = fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cities[randNum]}&limit=5&appid=4c71ba6fffb38a4baae0fe5623ee9a03`)
     .then( response => response.json())
     .then(data => {
         return data;
 });
 
+//Get temp for city based on coordinates
 const fetchTemp = fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=4c71ba6fffb38a4baae0fe5623ee9a03`)
         .then(function(res) {
             return res.json();
@@ -33,13 +35,27 @@ window.onload = async () => {
     console.log(cityName, temp);
 };
 
-    form.onsubmit = function (event) {
-         event.preventDefault();
-        let guess = this.elements.guess.value;
+//Refresh button
+const refreshButton = document.getElementById('refresh-button');
 
-     console.log(guess);
+const refreshPage = () => {
+  window.location.reload();
+}
 
-//     if (Number.isNaN(guess)) {
-//         alert ('That is not a number!');
-//     }
+refreshButton.addEventListener('click', refreshPage);
+
+//Get guess and match it
+form.onsubmit = function (event) {
+    event.preventDefault();
+    let guess = this.elements.guess.value;
+
+    console.log(guess);
+    
+        if (guess === temp){
+       alert('You are so good at this!');
+        }
+        else if (isNaN(guess)) {
+       alert('That is not a number!');
+        }
+    
 }
