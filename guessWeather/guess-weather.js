eb99f4543a1aaf61cd86f5d8f34892fc124b1da1
MalinW <maalinwallander@gmail.com>
2022-11-15 22:25:00 +0100

    message JS

--- guessWeather/guess-weather.js
+++ guessWeather/guess-weather.js
@@ -10,24 +10,35 @@ let submitButtonEl = document.getElementById('submit-button');
 let refreshbuttonEl = document.getElementById('refresh-button');
 let answerEl = document.getElementById('answer');
 let form = document.forms.guess;
+let messageEl = document.getElementById('answer');
 
 //Get random city and data for the city
 const fetchCity = fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cities[randNum]}&limit=5&appid=4c71ba6fffb38a4baae0fe5623ee9a03`)
-    .then( response => response.json())
+    .then(response => response.json())
     .then(data => {
         return data;
 });
+ 
+function showMessage(message) {
+    messageEl.innerHTML = message;
+ }
+//saving temp to be able to use it further down
+function getTemp() {
+    return localStorage.getItem("temp") || "";
+ }
 
 //Get temp for city based on coordinates
 const fetchTemp = (lat, long) => {
     return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=4c71ba6fffb38a4baae0fe5623ee9a03`)
         .then(function(res) {
             return res.json();
-            }).then(function(data) {
+            })
+            .then(function(data) {
                 let kelvin = data.main.temp;
                 return Math.floor(kelvin - 273.15);
-            });
-        }
+                })
+                .catch(err => showErrormessage("Sorry, something went wrong. Try again!"))
+                };
 
 window.onload = async () => {
     let data = await fetchCity;
@@ -36,6 +47,7 @@ window.onload = async () => {
     const lat = data[0].lat;
     const long = data[0].lon;
     let temp = await fetchTemp(lat, long);
+    localStorage.setItem("temp", temp);
     console.log(cityName, temp);
     };
 
@@ -52,7 +64,27 @@ refreshButton.addEventListener('click', refreshPage);
 form.onsubmit = function (event) {
     event.preventDefault();
     let guess = this.elements.guess.value;
+    let temp = getTemp();
 
-    console.log(guess);
+    //convert guess and temp to numbers
+    guess = Number(guess);
+    temp = Number(temp);
 
+    
+    if (guess === temp) {
+        showMessage("Success!");
+    }
+    else if (guess < temp) {
+        showMessage("Higher! Guess again");
+    }
+    else if (guess > temp) {
+        showMessage("Lower! Guess again");
+    }
+    else if (guess === NaN){
+        showMessage("That is not a number, guess again!");
+    }
+
+    function showMessage(message){
+        messageEl.innerHTML = message;
+    }
 }
