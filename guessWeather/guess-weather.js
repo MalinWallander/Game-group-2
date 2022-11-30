63439709852b632723a1a24ad784acb6474626ef
MalinW <maalinwallander@gmail.com>
2022-11-30 15:51:53 +0100

    changes based on Anna's notes

--- guessWeather/guess-weather.js
+++ guessWeather/guess-weather.js
@@ -1,27 +1,24 @@
 "use strict";
 
-import { cities, apiKey } from "./constants.js";
+import { cities} from "./constants.js";
 
 //Set random number
 let randNum = Math.floor(Math.random() * cities.length);
 //Set elements and objects
 let cityNameEl = document.getElementById('cityNamePresent');
-let submitButtonEl = document.getElementById('submit-button');
-let answerEl = document.getElementById('answer');
 let form = document.forms.guess;
 let messageEl = document.getElementById('answer');
+const unit = "metric"
 
 //Get random city and data for the city
 const fetchCity = fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${cities[randNum]}&limit=5&appid=4c71ba6fffb38a4baae0fe5623ee9a03`)
     .then(response => response.json())
     .then(data => {
         return data;
-});
+        })
+        .catch(err => showMessage("Sorry, something went wrong. Try again!"))
+        ;
 
-//function 
-function showMessage(message) {
-    messageEl.innerHTML = message;
- }
 //saving temp to be able to use it further down
 function getTemp() {
     return localStorage.getItem("temp") || "";
@@ -29,15 +26,15 @@ function getTemp() {
 
 //Get temp for city based on coordinates
 const fetchTemp = (lat, long) => {
-    return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=4c71ba6fffb38a4baae0fe5623ee9a03`)
+    return fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=${unit}&appid=4c71ba6fffb38a4baae0fe5623ee9a03`)
         .then(function(res) {
             return res.json();
             })
             .then(function(data) {
-                let kelvin = data.main.temp;
-                return Math.floor(kelvin - 273.15);
+                let temp = data.main.temp;
+                return Math.floor(temp);
                 })
-                .catch(err => showErrormessage("Sorry, something went wrong. Try again!"))
+                .catch(err => showMessage("Sorry, something went wrong. Try again!"))
                 };
 
 //wait for and set variables from the API-information fetched
@@ -55,17 +52,26 @@ window.onload = async () => {
 //Refresh button
 const refreshButton = document.getElementById('refresh-button');
 
+//function to refresh page
 const refreshPage = () => {
   window.location.reload();
 }
-
+//on click, run function refreshPage
 refreshButton.addEventListener('click', refreshPage);
+
 
 //Get guess
 form.onsubmit = function (event) {
     event.preventDefault();
+
     let guess = this.elements.guess.value;
     let temp = getTemp();
+
+
+    if (!guess) {
+        showMessage("Make a guess!");
+        return;
+      }
 
     //convert guess and temp to numbers
     guess = Number(guess);
@@ -75,17 +81,18 @@ form.onsubmit = function (event) {
     if (guess === temp) {
         showMessage("Success!");
     }
+    else if (isNaN(guess)){
+        showMessage("That is not a number, guess again!");
+    }
     else if (guess < temp) {
         showMessage("Higher! Guess again");
     }
     else if (guess > temp) {
         showMessage("Lower! Guess again");
     }
-    else if (guess === NaN){
-        showMessage("That is not a number, guess again!");
-    }
-
-    function showMessage(message){
-        messageEl.innerHTML = message;
-    }
 }
+
+//function to show message about guess
+function showMessage(message) {
+    messageEl.innerHTML = message;
+ }
--- snakeGame/snake.js
+++ snakeGame/snake.js
@@ -1,20 +1,14 @@
 "use strict";
-//Show the score and set it to start with 0
-document.addEventListener("DOMContentLoaded", function () {
-    let score = document.querySelector("div");
-    let newVal = document.createElement("p");
-    newVal.innerHTML = '';
-    score.appendChild(newVal);
-  });
 
-  let score = 0;
+//set starting score to 0
+let score = 0;
 
   //Set variables for the timer
 const timerEl = document.getElementById("timer");
 let timer;
 let timerStart;
   
-//create start button element to run main and gen_food function, start timer, and hide the button to start the game
+//create start button element to run main and genFood function, start timer, and hide the button to start the game
 let startButtonEl = document.getElementById("start-button");
 let refreshButtonEl = document.getElementById("refresh-button");
 
@@ -24,7 +18,7 @@ function startGame(){
     startButtonEl.classList.add("hidden");
     refreshButtonEl.classList.remove("hidden");
     main();
-    gen_food();
+    genFood();
     timer = setInterval(() => {
         updateTimer();
       }, 10);
@@ -86,156 +80,156 @@ document.addEventListener("keydown", change_direction);
 const gameOverBannerEl = document.getElementById("game-over-banner");
     
     // main function called repeatedly to keep the game running
-    function main() {  
-        if (has_game_ended()) {
-            gameOverBannerEl.classList.remove("hidden");
-            clearInterval(timer);
-            return;
-        }
-
-        else {
-            changing_direction = false;
-            setTimeout(function onTick() 
-            {    
-                clearCanvas(); 
-                drawGlobe();   
-                moveSnake();  
-                drawSnake();
-                // Call main again, recursive method
-                main();
-            }, 100)
-        }
+function main() {  
+    if (hasGameEnded()) {
+        gameOverBannerEl.classList.remove("hidden");
+        clearInterval(timer);
+        return;
     }
+
+    else {
+        changing_direction = false;
+        setTimeout(function onTick() 
+        {    
+          clearCanvas(); 
+          drawGlobe();   
+          moveSnake();  
+          drawSnake();
+            // Call main again, recursive method
+            main();
+        }, 100)
+    }
+}
     
-    // draw a border around the canvas
-    function clearCanvas() {
-      //  Select the colour to fill the drawing
-      snakeBoard_ctx.fillStyle = board_background;
-      //  Select the colour for the border of the canvas
-      snakeBoard_ctx.strokestyle = board_border;
-      // Draw a "filled" rectangle to cover the entire canvas
-      snakeBoard_ctx.fillRect(0, 0, snakeBoard.width, snakeBoard.height);
-      // Draw a "border" around the entire canvas
-      snakeBoard_ctx.strokeRect(0, 0, snakeBoard.width, snakeBoard.height);
-    }
-    
-    // Draw the snake on the canvas
-    function drawSnake() {
-      // Draw the locomotive in the first part of the train
-      drawLocomotive(snake[0]);
-      // Draw the rest of the train, slicing the first part
-      snake.slice(1).forEach(drawTraincart);
-    }
+// draw a border around the canvas
+function clearCanvas() {
+  //  Select the colour to fill the drawing
+  snakeBoard_ctx.fillStyle = board_background;
+  //  Select the colour for the border of the canvas
+  snakeBoard_ctx.strokestyle = board_border;
+  // Draw a "filled" rectangle to cover the entire canvas
+  snakeBoard_ctx.fillRect(0, 0, snakeBoard.width, snakeBoard.height);
+  // Draw a "border" around the entire canvas
+  snakeBoard_ctx.strokeRect(0, 0, snakeBoard.width, snakeBoard.height);
+}
 
-    //set the globe picture as food
-    function drawGlobe() {
-      let globe_image = new Image();
-      globe_image.src = "globe-2.png";
-      globe_image.onload = function(){
-        snakeBoard_ctx.drawImage(globe_image, food_x, food_y, 10, 10);
-      }
-    }
+// Draw the snake on the canvas
+function drawSnake() {
+  // Draw the locomotive in the first part of the train
+  drawLocomotive(snake[0]);
+  // Draw the rest of the train, slicing the first part
+  snake.slice(1).forEach(drawTraincart);
+}
 
-    //set the locomotive picture as the head of the snake-train
-    function drawLocomotive(snakePart){
-      let locomotive = new Image()
-      locomotive.src = "locomotive.webp";
-        locomotive.onload = function(){
-          snakeBoard_ctx.drawImage(locomotive, snakePart.x, snakePart.y, 10, 10)
+//set the globe picture as food
+function drawGlobe() {
+  let globe_image = new Image();
+  globe_image.src = "globe-2.png";
+  globe_image.onload = function(){
+    snakeBoard_ctx.drawImage(globe_image, food_x, food_y, 10, 10);
+  }
+}
+
+//set the locomotive picture as the head of the snake-train
+function drawLocomotive(snakePart){
+  let locomotive = new Image()
+  locomotive.src = "locomotive.webp";
+    locomotive.onload = function(){
+      snakeBoard_ctx.drawImage(locomotive, snakePart.x, snakePart.y, 10, 10)
+    }
+}
+
+//set the trincart image as part of the body of the snake-train
+function drawTraincart(snakePart){
+  let traincart = new Image()
+  traincart.src = "traincart.jpg";
+    traincart.onload = function(){
+      snakeBoard_ctx.drawImage(traincart, snakePart.x, snakePart.y, 10, 10)
+    }
+}
+
+//Set parameters to see if snake-train hits wall or self
+function hasGameEnded() {
+    for (let i = 4; i < snake.length; i++) {
+      if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true
+    }
+    const hitLeftWall = snake[0].x < 0;
+    const hitRightWall = snake[0].x > snakeBoard.width - 10;
+    const hitToptWall = snake[0].y < 0;
+    const hitBottomWall = snake[0].y > snakeBoard.height - 10;
+    return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall;
+  }
+
+  //Generate random number between min and max
+  function random_food(min, max) {
+    return Math.round((Math.random() * (max-min) + min) / 10) * 10;
+  }
+
+  function genFood() {
+    // Generate a random number the food x-coordinate
+    food_x = random_food(0, snakeBoard.width - 10);
+    // Generate a random number for the food y-coordinate
+    food_y = random_food(0, snakeBoard.height - 10);
+    // if the new food location is where the snake currently is, generate a new food location
+    snake.forEach(function hasSnakeEatenFood(part) {
+      const has_eaten = part.x == food_x && part.y == food_y;
+      if (has_eaten) genFood();
+    });
+  }
+
+  //Naming keys, connecting them to keyboard number
+function change_direction(event) {
+    const LEFT_KEY = 37;
+    const RIGHT_KEY = 39;
+    const UP_KEY = 38;
+    const DOWN_KEY = 40;
+
+    // Prevent the train from reversing
+
+    if (changing_direction) return;
+    // see which direction the snake-train is going
+    else {
+        changing_direction = true;
+        const keyPressed = event.keyCode;
+        const goingUp = dy === -10;
+        const goingDown = dy === 10;
+        const goingRight = dx === 10;
+        const goingLeft = dx === -10;
+      //change direction based on key pressed, if the snake-train isn't already going there
+        if (keyPressed === LEFT_KEY && !goingRight) {
+            dx = -10;
+            dy = 0;
+        }
+        if (keyPressed === UP_KEY && !goingDown) {
+            dx = 0;
+            dy = -10;
+        }
+        if (keyPressed === RIGHT_KEY && !goingLeft) {
+            dx = 10;
+            dy = 0;
+        }
+        if (keyPressed === DOWN_KEY && !goingUp) {
+            dx = 0;
+            dy = 10;
         }
     }
+}
 
-    //set the trincart image as part of the body of the snake-train
-    function drawTraincart(snakePart){
-      let traincart = new Image()
-      traincart.src = "traincart.jpg";
-        traincart.onload = function(){
-          snakeBoard_ctx.drawImage(traincart, snakePart.x, snakePart.y, 10, 10)
-        }
+function moveSnake() {
+    // Create the new Snake-train's head
+    const head = {x: snake[0].x + dx, y: snake[0].y + dy};
+    // Add the new head to the beginning of snake-train body
+    snake.unshift(head);
+    const has_eaten_food = snake[0].x === food_x && snake[0].y === food_y;
+    if (has_eaten_food) {
+      // Increase score
+      score += 10;
+      // Display score on screen
+      document.getElementById('score').innerHTML = score;
+      // Generate new food location
+      genFood();
+    } else {
+      // Remove the last part of snake-train body
+      snake.pop();
     }
-
-    //Set parameters to see if snake-train hits wall or self
-    function has_game_ended() {
-        for (let i = 4; i < snake.length; i++) {
-          if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true
-        }
-        const hitLeftWall = snake[0].x < 0;
-        const hitRightWall = snake[0].x > snakeBoard.width - 10;
-        const hitToptWall = snake[0].y < 0;
-        const hitBottomWall = snake[0].y > snakeBoard.height - 10;
-        return hitLeftWall || hitRightWall || hitToptWall || hitBottomWall;
-      }
-
-      //Generate random number between min and max
-      function random_food(min, max) {
-        return Math.round((Math.random() * (max-min) + min) / 10) * 10;
-      }
-  
-      function gen_food() {
-        // Generate a random number the food x-coordinate
-        food_x = random_food(0, snakeBoard.width - 10);
-        // Generate a random number for the food y-coordinate
-        food_y = random_food(0, snakeBoard.height - 10);
-        // if the new food location is where the snake currently is, generate a new food location
-        snake.forEach(function has_snake_eaten_food(part) {
-          const has_eaten = part.x == food_x && part.y == food_y;
-          if (has_eaten) gen_food();
-        });
-      }
-
-      //Naming keys, connecting them to keyboard number
-    function change_direction(event) {
-        const LEFT_KEY = 37;
-        const RIGHT_KEY = 39;
-        const UP_KEY = 38;
-        const DOWN_KEY = 40;
-
-        // Prevent the train from reversing
-    
-        if (changing_direction) return;
-        // see which direction the snake-train is going
-        else {
-            changing_direction = true;
-            const keyPressed = event.keyCode;
-            const goingUp = dy === -10;
-            const goingDown = dy === 10;
-            const goingRight = dx === 10;
-            const goingLeft = dx === -10;
-          //change direction based on key pressed, if the snake-train isn't already going there
-            if (keyPressed === LEFT_KEY && !goingRight) {
-                dx = -10;
-                dy = 0;
-            }
-            if (keyPressed === UP_KEY && !goingDown) {
-                dx = 0;
-                dy = -10;
-            }
-            if (keyPressed === RIGHT_KEY && !goingLeft) {
-                dx = 10;
-                dy = 0;
-            }
-            if (keyPressed === DOWN_KEY && !goingUp) {
-                dx = 0;
-                dy = 10;
-            }
-        }
-    }
-
-    function moveSnake() {
-        // Create the new Snake-train's head
-        const head = {x: snake[0].x + dx, y: snake[0].y + dy};
-        // Add the new head to the beginning of snake-train body
-        snake.unshift(head);
-        const has_eaten_food = snake[0].x === food_x && snake[0].y === food_y;
-        if (has_eaten_food) {
-          // Increase score
-          score += 10;
-          // Display score on screen
-          document.getElementById('score').innerHTML = score;
-          // Generate new food location
-          gen_food();
-        } else {
-          // Remove the last part of snake-train body
-          snake.pop();
-        }
-      }
+  }
